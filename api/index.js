786a609804167f42cb0c0828d62271b2d6cdef6f
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2025-04-10 17:11:45 -0300

    api fixed

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(require('../db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+// Exportar como función handler para Vercel
+module.exports = (req, res) => {
+  server(req, res);
+};
+
